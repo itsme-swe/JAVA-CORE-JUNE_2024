0ce35528e05c57326237988b85fb68b8c82c1dab
Harsh Mehra <me.harshmehra07@gmail.com>
2024-06-28 22:16:28 +0530

    New Commit

--- src/ClientAndServerCommunication/CommunicationTwo/FactorialClient.java
+++ src/ClientAndServerCommunication/CommunicationTwo/FactorialClient.java
@@ -1,7 +1,37 @@
 package ClientAndServerCommunication.CommunicationTwo;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class FactorialClient {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Client Started");
+
+        //1️⃣ Step
+        Socket soc = new Socket("localhost", 5000);
+
+        //2️⃣ Step
+        BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+
+        //3️⃣ Step
+        System.out.print("Enter the number: ");
+
+        //4️⃣ Converting string into integer
+        int number = Integer.parseInt(userInput.readLine());
+
+        //5️⃣ Step
+        PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+        out.println(number);
+
+        //6️⃣ Step
+        BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+        System.out.println(in.readLine());
+
+        soc.close();
+
 
     }
 }
--- src/ClientAndServerCommunication/CommunicationTwo/FactorialServer.java
+++ src/ClientAndServerCommunication/CommunicationTwo/FactorialServer.java
@@ -1,7 +1,43 @@
 package ClientAndServerCommunication.CommunicationTwo;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class FactorialServer {
-    public static void main(String[] args) {
+
+    //🔸Function which will calculate factorial
+    static int calculateFactorial(int number)
+    {
+        int fact = 1;
+        for (int i = 1; i <= number; i++){
+            fact = fact * i;
+        }
+        return fact;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Waiting for client...");
+
+        //1️⃣ Step
+        ServerSocket ss = new ServerSocket(5000);
+        Socket soc = ss.accept();
+
+        //2️⃣ Step
+        BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+        int number = Integer.parseInt(in.readLine());
+
+        //3️⃣ Step
+        PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+        out.println("Factorial of " + number + " is " + calculateFactorial(number));
+
+        soc.close();
+
 
     }
 }
+
+
