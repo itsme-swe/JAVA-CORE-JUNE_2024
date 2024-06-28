f96bb808455a48db90092628ff17a033dc216e61
Harsh Mehra <me.harshmehra07@gmail.com>
2024-06-28 21:48:40 +0530

    New Commit

--- src/ClientAndServerCommunication/CommunicationOne/EchoClient.java
+++ src/ClientAndServerCommunication/CommunicationOne/EchoClient.java
@@ -1,7 +1,33 @@
 package ClientAndServerCommunication.CommunicationOne;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class EchoClient {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("client Started");
+
+        Socket soc = new Socket("localhost", 3000);
+
+        /*1️⃣ Taking input from keyboard.
+            Converting bytes to char by using "InputStreamReader()"
+            and then converting individual char into whole string by using "BufferedReader()" */
+        BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter your msg: ");
+        String str = userInput.readLine();      //🔸 "readLine()"method will read line of text from input stream and storing in String reference
+
+        //2️⃣ Creating an object of PrintWriter to send string to the server
+        PrintWriter out = new PrintWriter(soc.getOutputStream(), true);     //🔸"true" is for Auto flush
+        out.println(str);
+
+        //3️⃣ Now in last reading the data sent by the server and displaying on screen
+        BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+        System.out.println(in.readLine());
+
 
     }
 }
--- src/ClientAndServerCommunication/CommunicationOne/EchoServer.java
+++ src/ClientAndServerCommunication/CommunicationOne/EchoServer.java
@@ -1,7 +1,35 @@
 package ClientAndServerCommunication.CommunicationOne;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class EchoServer {
     public static void main(String[] args) {
 
+        System.out.println("Waiting for client...");
+
+        try {
+            //1️⃣ Binding ServerSocket with the port number 3000
+            ServerSocket ss = new ServerSocket(3000);
+            Socket soc = ss.accept();
+            System.out.println("Connection Established");
+
+            //2️⃣ Reading the string sent by the client so reading string from client Socket using getInputStream()
+            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+
+            String str = in.readLine();
+
+            //3️⃣ Sending string back to the client
+            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+            out.println("Server says " + str);
+
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+        }
     }
 }
