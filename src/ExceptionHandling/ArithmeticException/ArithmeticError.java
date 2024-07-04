f3e2c12efd709093eded1f956995f422c3c22cfc
Harsh Mehra <me.harshmehra07@gmail.com>
2024-07-04 06:59:51 +0530

    Update

--- src/ExceptionHandling/ArithmeticException/ArithmeticError.java
+++ src/ExceptionHandling/ArithmeticException/ArithmeticError.java
@@ -1,20 +1,30 @@
 package ExceptionHandling.ArithmeticException;
 
+import java.util.Scanner;
+
 public class ArithmeticError {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        int a, b, c;
+
+        System.out.println("Enter the value of a: ");
+        a = sc.nextInt();
+
+        System.out.println("Enter the value of b: ");
+        b = sc.nextInt();
+
         try {
-            int a = 20;
-            int b = 0;
 
-            int c = a / b;
+            c = a / b;
 
-            System.out.println("Result is: " + c);
+            System.out.print("The division of a and b is " + c);
 
         }
         catch (ArithmeticException e){
-            System.out.println("Division by Zero");
+            System.out.println("Division by Zero " + e );
         }
     }
 }
