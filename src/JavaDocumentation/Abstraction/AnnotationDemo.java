25dea1ec853baa5570e775f76ce77024cf8bdb54
Harsh Mehra <me.harshmehra07@gmail.com>
2024-07-09 20:33:46 +0530

    Update

---
+++ src/JavaDocumentation/Abstraction/AnnotationDemo.java
@@ -0,0 +1,30 @@
+package JavaDocumentation.Abstraction;
+
+abstract class Parent
+{
+    abstract public void display();
+}
+
+
+class Child extends Parent
+{
+    @Override
+    public void display()
+    {
+
+    }
+
+    @Deprecated
+    public void show()
+    {
+        System.out.println("Hi");
+    }
+}
+public class AnnotationDemo {
+    @SuppressWarnings("deprecation")
+    public static void main(String[] args) {
+
+        Child c1 = new Child();
+        c1.show();      //🔸 if we use @Deprecated annotation on method then our compiler will give us suggestion not to use that method.
+    }
+}
